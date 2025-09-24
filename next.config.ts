fc27b1abcc933c9c9e564b2417910b532725a553
kentoapplication <kentoapplication@gmail>
2025-09-24 21:20:45 +0900

    fix: githubpages to vercel

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/dooodle-support',
-  assetPrefix: '/dooodle-support/',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true,
   },
-  
-  // async rewrites() {
-  //   return [
-  //     {
-  //       "source": "!.well-known/**",
-  //       "destination": "index.html"
-  //     },
-  //     {
-  //       "source": ".well-known/apple-app-site-association",
-  //       "destination": ".well-known/apple-app-site-association.json"
-  //     }
-  //   ]
-  // },
-
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/dooodle-support/.well-known/apple-app-site-association.json',
-  //       headers: [
-  //         {
-  //           key: 'Content-Type',
-  //           value: 'application/json',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
-
   typescript: {
     ignoreBuildErrors: false,
   },
